1a59b117a6c0bbb9252ee087b708f3d74688df77
amaliujia <amaliujia@163.com>
2014-12-16 13:17:54 -0500

    [SD Communication] add test file

--- src/SDUtil.java
+++ src/SDUtil.java
@@ -4,6 +4,10 @@ import static java.lang.System.exit;
  * Created by amaliujia on 14-12-13.
  */
 public class SDUtil {
+
+    static String inputFilePath = "input/WhatIf.txt";
+    static String outputFilePath = "output/WahtIf.txt";
+
     static void fatalError (String message) {
         System.err.println (message);
         exit(1);
