1d4546d6530a24d8b87c467f50d7257fc0716120
amaliujia <amaliujia@163.com>
2014-12-16 12:00:11 -0500

    [SD Commnication] add transactional IO Components

--- src/TransactionalFileInputStream.java
+++ src/TransactionalFileInputStream.java
@@ -1,18 +1,71 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * Created by amaliujia on 14-12-14.
  */
 public class TransactionalFileInputStream extends InputStream implements Serializable {
 
+    private String fileName;
+    private RandomAccessFile randomAccessFile;
+    private long offset;
+    private boolean migratable;
+
     public TransactionalFileInputStream(String arg){
+        this.fileName = arg;
+
+        try{
+            this.randomAccessFile = new RandomAccessFile(this.fileName, "rws");
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot find file " + this.fileName + " in file system");
+            e.printStackTrace();
+        } catch (IllegalArgumentException e){
+            System.err.println("illegal arguments");
+            e.printStackTrace();
+        } catch (SecurityException e){
+            System.err.println("Security problem");
+            e.printStackTrace();
+        }
+
+        offset = 0L;
+        migratable = false;
+    }
+
+
+    public int read() throws IOException {
+        if(migratable){
+            this.randomAccessFile = new RandomAccessFile(this.fileName, "rws");
+            migratable = false;
+        }
+
+        int readBytes;
+        this.randomAccessFile.seek(offset);
+        readBytes = this.randomAccessFile.read();
+        if(readBytes != -1){
+           offset++;
+        }
+
+        return readBytes;
+    }
+
+    public void closeFileStream(){
+
+        try {
+            this.randomAccessFile.close();
+        } catch (IOException e) {
+             e.printStackTrace();
+        }
 
     }
 
-    @Override
-    public int read() throws IOException {
-        return 0;
+    //TODO: check spelling
+    public boolean isMigaratable(){
+         if(migratable){
+             return true;
+         }
+         return false;
+    }
+
+    public long getOffset(){
+        return offset;
     }
 }
--- src/TransactionalFileOutputStream.java
+++ src/TransactionalFileOutputStream.java
@@ -1,18 +1,68 @@
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.Serializable;
+import java.io.*;
+
 
 /**
  * Created by amaliujia on 14-12-14.
  */
 public class TransactionalFileOutputStream extends OutputStream implements Serializable {
+    private String fileName;
+    private RandomAccessFile randomAccessFile;
+    private long offset;
+    private boolean migaratable;
 
     public TransactionalFileOutputStream(String arg, boolean b){
+         this.fileName = arg;
+         try{
+             this.randomAccessFile = new RandomAccessFile(this.fileName, "rws");
+         } catch (FileNotFoundException e) {
+             System.err.println("Cannot find file " + this.fileName + " in file system");
+         } catch (IllegalArgumentException e){
+             System.err.println("illegal arguments");
+         } catch (SecurityException e){
+             System.err.println("Security problem");
+         }
+
+         offset = 0L;
+         migaratable = false;
 
     }
 
-    @Override
-    public void write(int b) throws IOException {
 
+    public void write(int b) throws IOException {
+        if(migaratable){
+            this.randomAccessFile = new RandomAccessFile(this.fileName, "rws");
+            migaratable = false;
+        }
+
+        int readBytes;
+        this.randomAccessFile.seek(offset);
+        try {
+            this.randomAccessFile.write(b);
+        }catch (IOException e){
+            System.err.println("Fail to write to " + this.fileName);
+            e.printStackTrace();
+        }
+    }
+
+    public void closeFileStream(){
+
+        try {
+            this.randomAccessFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //TODO: check spelling
+    public boolean isMigaratable(){
+        if(migaratable){
+            return true;
+        }
+        return false;
+    }
+
+    public long getOffset(){
+        return offset;
     }
 }
