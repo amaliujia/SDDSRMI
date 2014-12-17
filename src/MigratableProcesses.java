3a8280b51923c835bb4600fda5db178d74c66a4e
amaliujia <amaliujia@163.com>
2014-12-16 20:43:46 -0500

    [SD Communication] add condition to judge if processes finish

--- src/MigratableProcesses.java
+++ src/MigratableProcesses.java
@@ -6,8 +6,13 @@ import java.io.Serializable;
 
 public interface MigratableProcesses extends Runnable, Serializable{
 
+    // file operation
     public abstract void run();
 
+    // suspending this function
     public abstract void suspend();
+
+    // check if this process finished
+    public boolean finished();
 
 }
