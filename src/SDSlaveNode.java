90d1b429b22b97e71e1e0560e6f1506baf259d88
Kang Huang <kanghuangnj@gmail.com>
2014-12-16 21:39:08 -0500

    [hk communication slavenode]

--- src/SDSlaveNode.java
+++ src/SDSlaveNode.java
@@ -9,8 +9,8 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.TreeMap;
 public class SDSlaveNode {
-    private OutputStreamWriter osw;
-    private BufferedReader bis;
+    private PrintWriter pw;
+    private BufferedReader bs;
     private String masterAddress;
     private int masterPort;
     private Socket socket;
@@ -25,8 +25,8 @@ public class SDSlaveNode {
     public void connect(){
         try{
             socket = new Socket(masterAddress, masterPort);
-            bis = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            osw = new OutputStreamWriter(socket.getOutputStream());
+            bs = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
         }
         catch(IOException ex) {
             System.out.println("MasterAddress or MasterPort is wrong");
@@ -36,8 +36,8 @@ public class SDSlaveNode {
 
     public void disconnect(){
         try{
-            bis.close();
-            osw.close();
+            bs.close();
+            pw.close();
             socket.close();
         }
         catch (IOException ex)
@@ -48,16 +48,47 @@ public class SDSlaveNode {
     }
 
 
-    public void slaveService()throws IOException{
+    public void slaveService()throws IOException, ClassNotFoundException{
         String[] args = null;
         String command;
-        while((command = bis.readLine())!= null ){
+        while((command = bs.readLine())!= null ){
             args = command.split(" ");
             if (args[0].equals("ps")){
                 for (Integer processID : processTable.keySet()){
                     SDprocessInfo singleProcess = processTable.get(processID);
-                    if (singleProcess.process)
+                    if (singleProcess.process.finished()){
+                        pw.write("$ " + processID +  "   " + SDProcessStatus.TERMINATED + "\n");
+                    }
+                    else{
+                        pw.write("$ " + processID +  "   " + singleProcess.status +  "\n");
+                    }
                 }
+            }
+            else if (args[0].equals("resume")){
+                FileInputStream in = new FileInputStream(args[1] + args[2] + args[3] + ".obj");
+                ObjectInputStream inObj = new ObjectInputStream(in);
+                SDMigratableProcess mpIn = (SDMigratableProcess)inObj.readObject();
+                in.close();
+                inObj.close();
+                Thread newProcess = new Thread(mpIn);
+                SDprocessInfo processInfo = new SDprocessInfo(SDProcessStatus.RUNNING, mpIn);
+                this.processTable.put(this.processID, processInfo);
+                this.processID++;
+                newProcess.start();
+            }
+            else if (args[0].equals("suspend")){ // suspend
+                int migratableProcessID = -1;
+                try{
+                    migratableProcessID = Integer.parseInt(args[1]);
+                }
+                catch(NumberFormatException ex){
+                    System.err.println("process not found! Please check.");
+                }
+                SDprocessInfo processInfo = this.processTable.get(migratableProcessID);
+                FileOutputStream out = new FileOutputStream(args[1] + ".obj");
+                ObjectOutputStream outObj = new ObjectOutputStream(out);
+                outObj.writeObject(processInfo.process);
+                outObj.flush();
             }
             else if (args[0].equals("exit")){
 
